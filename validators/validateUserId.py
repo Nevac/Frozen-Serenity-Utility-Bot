1c36c7fa81d7bb40ae1e93c68c1d84d373e4de74
RolandSchmid <RolandSchmid@users.noreply.github.com>
2020-06-29 15:46:56 +0200

    Handle usernames in messages

--- validators/validateUserId.py
+++ validators/validateUserId.py
@@ -3,16 +3,21 @@ from exceptions.userNotFound import UserNotFound
 from validators.validateIntAsString import validate_int_as_string
 
 
-def validate_user_id(string):
+def validate_user_id(key: str) -> int:
+    if type(key) is int:
+        return int(key)
     try:
-        validate_text(string)
+        validate_text(key)
 
-        if string.startswith('<@!') and string.endswith('>'):
-            string = string[3:-1]
-        elif string.startswith('<@') and string.endswith('>'):
-            string = string[2:-1]
+        if key.startswith('<@!') and key.endswith('>'):
+            key = key[3:-1]
+        elif key.startswith('<@') and key.endswith('>'):
+            key = key[2:-1]
+        elif key.isnumeric():
+            pass
         else:
             raise ValueError()
-        validate_int_as_string(string)
+        validate_int_as_string(key)
+        return int(key)
     except Exception:
         raise UserNotFound
