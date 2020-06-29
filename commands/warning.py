43b2e3f26c2792d3f76966f6e9441f2335aabc79
RolandSchmid <RolandSchmid@users.noreply.github.com>
2020-06-29 15:46:56 +0200

    Handle usernames in messages

--- commands/warning.py
+++ commands/warning.py
@@ -2,7 +2,8 @@ from exceptions.commandIncomplete import CommandIncomplete
 from exceptions.userNotFound import UserNotFound
 from services.localizationService import i18n
 
-from util.utility import extract_user_id, get_user, get_warnings, add_warning, get_warnings_top
+from util.utility import get_user, is_user, add_warning, get_warnings, get_warnings_top
+from validators.validateUserId import validate_user_id
 
 
 async def warning(message, client):
@@ -10,20 +11,21 @@ async def warning(message, client):
 
     try:
         validate_warning_user_argument(command)
-        id_string = command[2]
-        _id = extract_user_id(id_string)
+        target_id = validate_user_id(command[2])
+        owner_id = message.author.id
 
-        if _id == client.user.id:
-            _id = message.author.id
+        if target_id == client.user.id:
+            target_id = message.author.id
+            owner_id = client.user.id
             dialog = i18n.t('dialogs.warning.give_bot')
-        elif _id == message.author.id:
+        elif target_id == message.author.id:
             dialog = i18n.t('dialogs.warning.give_self')
         else:
             dialog = i18n.t('dialogs.warning.give')
 
-        target = get_user(_id, client)
-        author = get_user(message.author.id, client)
-        add_warning(author, target, ' '.join(command[3:]))
+        target = get_user(client, target_id)
+        author = get_user(client, owner_id)
+        add_warning(author, target, ' '.join(command[3:]).strip())
 
         await message.channel.send(dialog.format(get_warnings(target)))
     except UserNotFound as e:
@@ -36,7 +38,7 @@ async def warnings(message, client):
     command = message.content.split(' ')
 
     try:
-        user = get_user(message.author.id, client)
+        user = get_user(client, message.author.id)
         top = int(command[2]) if len(command) == 3 and command[2].isnumeric() else 5
         if len(command) == 3 and not command[2].isnumeric():
             dialog = i18n.t('dialogs.warning.check_invalid').format(get_warnings(user), command[2])
@@ -45,15 +47,18 @@ async def warnings(message, client):
 
         top_warnings = get_warnings_top(user, top)
         dialog += '```' + '\n'.join([
-            w.date.strftime("%d.%m.%Y %H:%M:%S") + ' \t'
-            + w.owner.name + ' \t'
-            # TODO Resolve user IDs in reason
-            + w.reason
+            w.date.strftime("%d.%m.%Y %H:%M:%S") + ' '
+            + w.owner.name.ljust(25) + ' '
+            + __resolve_users(client, w.reason)
             for w in top_warnings]) + '```'
 
         await message.channel.send(dialog)
     except UserNotFound as e:
         await message.channel.send(e.message)
+
+
+def __resolve_users(client, words: str):
+    return ' '.join(['@' + user.name if (user := is_user(client, w)) is not None else w for w in words.split(' ')])
 
 
 def validate_warning_user_argument(command: list):
--- util/utility.py
+++ util/utility.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from discord import Client
 from mongoengine import DoesNotExist
 
 from documents.reason import Reason
@@ -8,9 +9,9 @@ from exceptions.userNotFound import UserNotFound
 from validators.validateUserId import validate_user_id
 
 
-def get_user(_id, client):
-    discord_user = client.get_user(_id)
-
+def get_user(client: Client, _id: str) -> User:
+    user_id = validate_user_id(_id)
+    discord_user = client.get_user(user_id)
     if discord_user is None:
         raise UserNotFound()
 
@@ -23,20 +24,19 @@ def get_user(_id, client):
     return user
 
 
-def extract_user_id(string: str) -> int:
-    validate_user_id(string)
-    if string.startswith('<@!'):
-        return int(string[3:-1])
-    else:
-        return int(string[2:-1])
+def is_user(client: Client, key: str) -> User or None:
+    try:
+        return get_user(client, key)
+    except UserNotFound as e:
+        return None
 
 
-def add_warning(owner: User, target: User, reason: str):
+def add_warning(owner: User, target: User, reason: str) -> None:
     reason = Reason(date=datetime.now, owner=owner, target=target, reason=reason)
     reason.save()
 
 
-def get_warnings(user: User):
+def get_warnings(user: User) -> int:
     # TODO Backwards compatibility, should old warnings still be counted?
     return Reason.objects.filter(target=user).count() + user.warnings
 
